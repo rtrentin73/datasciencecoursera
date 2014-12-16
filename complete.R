752111dd353f94466b4612a65d13dfd9d5207f7a
Ricardo Trentin <rtrentin73@gmail.com>
2014-12-15 20:17:53 -0500

    Changes to be committed: new file:   complete.R new file:   corr.R new file:   pollutantmean.R

---
+++ complete.R
@@ -0,0 +1,22 @@
+complete <- function(directory, id = 1:332) {
+  ## 'directory' is a character vector of length 1 indicating
+  ## the location of the CSV files
+  
+  ## 'id' is an integer vector indicating the monitor ID numbers
+  ## to be used
+  
+  ## Return a data frame of the form:
+  ## id nobs
+  ## 1  117
+  ## 2  1041
+  ## ...
+  ## where 'id' is the monitor ID number and 'nobs' is the
+  ## number of complete cases
+  if (getwd() != "/Users/rtrentin/bigdata/rprogramming/specdata" ) {
+    setwd(paste("/Users/rtrentin/bigdata/rprogramming/", directory, sep =""))
+  }
+  nobs <- function(id) {
+    sum(complete.cases(read.csv(paste(sprintf("%03d", id), ".csv", sep=""))))
+  }
+  data.frame(id = id, nobs = sapply(id,nobs))
+}
